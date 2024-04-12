20e7f0912d8793b3b88195af51be0f2ce51bf52a
ALPHA\USER <vallesmark15@gmail.com>
2024-04-13 03:05:17 +0800

    Staging commit

--- build/utils/Messages.js
+++ build/utils/Messages.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Messages = void 0;
 exports.Messages = {
     //ERRORS
-    'LOGIN_FAILED': "Login Failed!"
+    'SIMPLE_FAILED': "Failed",
+    'SIMPLE_SUCCESS': "Success"
 };
