56436bff0fe06582e856dd0418aa2a9fa6c4c8c0
ALPHA\USER <vallesmark15@gmail.com>
2024-04-13 03:07:46 +0800

    remove build

--- build/utils/Messages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Messages = void 0;
-exports.Messages = {
-    //ERRORS
-    'LOGIN_FAILED': "Login Failed!"
-};
