9cb0e2a5d31490ad9c098f431fffe88875c3da74
ALPHA\USER <vallesmark15@gmail.com>
2024-06-15 02:07:56 +0800

    PATCH::add cors

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,40 @@ import bodyParser from 'body-parser'
 
 import UserController from './controller/UsersController'
 import LoginController from './controller/LoginController'
+import cors from 'cors'
 
 import verifyToken from './middleware/VerifyToken'
 
-async function startServer() { 
+async function startServer() {
     try {
 
-      await connectDB()
+        await connectDB()
 
-      const app = express()
-      app.use(morgan('dev'))
-      app.use(bodyParser.json())
-
-      const port = process.env.PORT || 3000
-      const apiRouter = express.Router()
-      
-      // Define routes
-      apiRouter.use('/login', LoginController)
-      apiRouter.use('/users',verifyToken, UserController)
-
-
-      app.use('/api', apiRouter)
+        const app = express()
+        app.use(morgan('dev'))
+        app.use(bodyParser.json())
+        // Configure CORS to allow requests from http://localhost:3001
+        const corsOptions = {
+            origin: 'http://localhost:3001',
+            optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+        };
+        app.use(cors());
 
 
-      app.listen(port, () => {
-          console.log(`Server running at ${process.env.HOST}:${port}`)
-      })
+
+        const port = process.env.PORT || 3000
+        const apiRouter = express.Router()
+
+        // Define routes
+        apiRouter.use('/login', LoginController)
+        apiRouter.use('/users', verifyToken, UserController)
+
+        app.use('/api', apiRouter)
+
+
+        app.listen(port, () => {
+            console.log(`Server running at ${process.env.HOST}:${port}`)
+        })
 
     } catch (error) {
         console.error('Failed to start server:', error)
